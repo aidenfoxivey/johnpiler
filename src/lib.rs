ee2bbbb09dc6d764090ec44bf1525539c7c16d86
Aiden Fox Ivey <aiden@aidenfoxivey.com>
2025-01-30 18:32:07 -0500

    Add basic test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 // For quick access, here's the R5RS Standard.
 // https://conservatory.scheme.org/schemers/Documents/Standards/R5RS/HTML/
 
+#[derive(Debug, PartialEq)]
 enum TokenKind {
     LParen,     // (
     RParen,     // )
@@ -47,6 +48,7 @@ enum TokenKind {
     Ellipsis, // ...
 }
 
+#[derive(Debug)]
 struct Token {
     column: usize,
     line: usize,
@@ -61,7 +63,7 @@ struct Lexer {
 }
 
 impl Lexer {
-    pub fn new(input: String) -> Self {
+    pub fn new(input: &str) -> Self {
         Self {
             source: input.chars().collect(),
             // I do this here because doing input.chars().nth() everytime I need
@@ -73,15 +75,52 @@ impl Lexer {
         }
     }
 
-    pub fn lex() -> Vec<Token> {
-        todo!();
+    pub fn lex(&mut self) -> Vec<Token> {
+        let mut v = vec![];
+        while !self.is_at_end() {
+            v.push(self.scan_token());
+        }
+        v
     }
 
     fn is_at_end(&self) -> bool {
-        self.source.len() >= self.index
+        self.index >= self.source.len()
     }
 
     fn peek(&self) -> char {
+        self.source.get(self.index + 1).copied().unwrap_or('\0')
+    }
+
+    fn bite(&self) -> char {
         self.source.get(self.index).copied().unwrap_or('\0')
+    }
+
+    fn scan_token(&mut self) -> Token {
+        let kind = match self.bite() {
+            '(' => TokenKind::LParen,
+            '\0' => TokenKind::Eof,
+            _ => todo!(),
+        };
+
+        let t = Token {
+            column: self.column,
+            line: self.line,
+            kind,
+        };
+
+        self.index += 1;
+
+        t
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lexer_canonical() {
+        let mut l = Lexer::new("(+ 2 3)");
+        let v = l.lex();
+        assert_eq!(v[0].kind, TokenKind::LParen);
     }
 }

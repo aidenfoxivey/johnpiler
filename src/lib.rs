842318fcec878ffbf72cc25088d4bcc8143385ba
Aiden Fox Ivey <aiden@aidenfoxivey.com>
2025-01-30 18:55:21 -0500

    Add some other tests

--- src/lib.rs
+++ src/lib.rs
@@ -78,7 +78,9 @@ impl Lexer {
     pub fn lex(&mut self) -> Vec<Token> {
         let mut v = vec![];
         while !self.is_at_end() {
-            v.push(self.scan_token());
+            if let Some(tok) = self.scan_token() {
+                v.push(tok);
+            }
         }
         v
     }
@@ -91,36 +93,70 @@ impl Lexer {
         self.source.get(self.index + 1).copied().unwrap_or('\0')
     }
 
-    fn bite(&self) -> char {
-        self.source.get(self.index).copied().unwrap_or('\0')
+    fn bite(&mut self) -> char {
+        let c = self.source.get(self.index).copied().unwrap_or('\0');
+        self.index += 1;
+        c
     }
 
-    fn scan_token(&mut self) -> Token {
-        let kind = match self.bite() {
-            '(' => TokenKind::LParen,
-            '\0' => TokenKind::Eof,
-            _ => todo!(),
-        };
-
-        let t = Token {
+    fn make_token(&mut self, kind: TokenKind) -> Option<Token> {
+        let token: Token = Token {
             column: self.column,
             line: self.line,
             kind,
         };
-
         self.index += 1;
+        Some(token)
+    }
 
-        t
+    fn scan_token(&mut self) -> Option<Token> {
+        match self.bite() {
+            '(' => self.make_token(TokenKind::LParen),
+            ')' => self.make_token(TokenKind::RParen),
+            '-' => self.make_token(TokenKind::Minus),
+            '+' => self.make_token(TokenKind::Plus),
+            '\0' => self.make_token(TokenKind::Eof),
+            '#' => {
+                let next = self.bite();
+                if next == 't' {
+                    return self.make_token(TokenKind::True);
+                } else if next == 'f' {
+                    return self.make_token(TokenKind::False);
+                } else if next == '(' {
+                    return self.make_token(TokenKind::HashLParen);
+                }
+
+                todo!();
+            }
+            ' ' | '\t' => None,
+            '\n' => {
+                self.line += 1;
+                None
+            }
+            _ => todo!(),
+        }
     }
 }
 
+#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn lexer_canonical() {
-        let mut l = Lexer::new("(+ 2 3)");
-        let v = l.lex();
+        let v = Lexer::new("(+ 2 3)").lex();
         assert_eq!(v[0].kind, TokenKind::LParen);
+    }
+
+    #[test]
+    fn lexer_true() {
+        // Sorry, I know this isn't a valid AST for scheme. (At least I don't
+        // this it is.)
+        let v = Lexer::new("( #t #f )").lex();
+        assert_eq!(v.len(), 4);
+        assert_eq!(v[0].kind, TokenKind::LParen);
+        assert_eq!(v[1].kind, TokenKind::True);
+        assert_eq!(v[2].kind, TokenKind::False);
+        assert_eq!(v[3].kind, TokenKind::RParen);
     }
 }
